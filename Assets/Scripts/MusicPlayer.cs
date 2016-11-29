ac92c2c8dbb0772954a7fa1fb5e47fce6a65da4c
dfatih <doenmezfatih@outlook.com>
2016-11-29 01:07:44 +0100

    singleton

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -3,10 +3,25 @@ using System.Collections;
 
 public class MusicPlayer : MonoBehaviour {
 
+    static MusicPlayer instance=null;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            DestroyImmediate(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(instance);
+        }
+    }
+
 	// Use this for initialization
 	void Start () {
-        GameObject.DontDestroyOnLoad(gameObject);
-	}
+        
+    }
 	
 	// Update is called once per frame
 	void Update () {
